ba1e0035d9d890bd7ce9fdbdec9de122e15a9a5e
EZpete002 <peterleefeitosa@gmail.com>
2025-08-07 18:13:43 -0500

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,3 @@
+def scrape_passenger_info(record_locator, last_name):
+    # ⛔ TEMP: replace this with real scraper later
+    return f"Passenger {last_name} with locator {record_locator} missed DFW to MIA. Next leg active to GIG."
